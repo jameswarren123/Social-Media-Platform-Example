4ef3689fb726daa017c688f24b398bc9faed30ee
Jiwon Park <98673424+jiwonp2003@users.noreply.github.com>
2025-03-19 07:36:19 +0000

    Profile Displays Posts

--- src/main/java/uga/menik/cs4370/controllers/ProfileController.java
+++ src/main/java/uga/menik/cs4370/controllers/ProfileController.java
@@ -5,6 +5,7 @@ This is a project developed by Dr. Menik to give the students an opportunity to 
 */
 package uga.menik.cs4370.controllers;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import uga.menik.cs4370.models.Post;
+import uga.menik.cs4370.services.PostService;
 import uga.menik.cs4370.services.UserService;
 import uga.menik.cs4370.utility.Utility;
 
@@ -27,54 +29,54 @@ public class ProfileController {
 
     // UserService has user login and registration related functions.
     private final UserService userService;
-
+    private final PostService postService;
     /**
      * See notes in AuthInterceptor.java regarding how this works 
      * through dependency injection and inversion of control.
      */
     @Autowired
-    public ProfileController(UserService userService) {
+    public ProfileController(UserService userService, PostService postService) {
         this.userService = userService;
+        this.postService = postService;
     }
 
     /**
      * This function handles /profile URL itself.
      * This serves the webpage that shows posts of the logged in user.
-     */
-    @GetMapping
-    public ModelAndView profileOfLoggedInUser() {
-        System.out.println("User is attempting to view profile of the logged in user.");
+          * @throws SQLException 
+          */
+         @GetMapping
+         public ModelAndView profileOfLoggedInUser() throws SQLException {
+        System.out.println("User is attempting to view profile of the logged-in user.");
         return profileOfSpecificUser(userService.getLoggedInUser().getUserId());
     }
 
     /**
-     * This function handles /profile/{userId} URL.
-     * This serves the webpage that shows posts of a speific user given by userId.
-     * See comments in PeopleController.java in followUnfollowUser function regarding 
-     * how path variables work.
+     * Handles the request for viewing a specific user's profile page.
+     * This method is invoked when a user clicks on another user's username
+     * on the people page. It fetches the posts of the specified user and 
+     * returns them in a ModelAndView object to be displayed on the profile page.
+     * 
+     * @param userId The ID of the user whose profile is being viewed.
+     * @return A ModelAndView object containing the view name ("posts_page") and 
+     *         the user's posts to be displayed in the view.
+     * @throws SQLException If there is an issue querying the database for posts.
      */
     @GetMapping("/{userId}")
-    public ModelAndView profileOfSpecificUser(@PathVariable("userId") String userId) {
+    public ModelAndView profileOfSpecificUser(@PathVariable("userId") String userId) throws SQLException {
         System.out.println("User is attempting to view profile: " + userId);
         
-        // See notes on ModelAndView in BookmarksController.java.
         ModelAndView mv = new ModelAndView("posts_page");
 
-        // Following line populates sample data.
-        // You should replace it with actual data from the database.
-        List<Post> posts = Utility.createSamplePostsListWithoutComments();
+        // Fetch actual posts from the database
+        List<Post> posts = postService.getUserPosts(userId); 
+
         mv.addObject("posts", posts);
 
-        // If an error occured, you can set the following property with the
-        // error message to show the error message to the user.
-        // String errorMessage = "Some error occured!";
-        // mv.addObject("errorMessage", errorMessage);
+        if (posts.isEmpty()) {
+            mv.addObject("isNoContent", true);
+        }
 
-        // Enable the following line if you want to show no content message.
-        // Do that if your content list is empty.
-        // mv.addObject("isNoContent", true);
-        
         return mv;
     }
-    
 }
